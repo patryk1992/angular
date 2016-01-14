09e2a9a90aa0f3509194f29ee623a301b7dc0161
Wojciech Mich <wojciech.mich.wm@gmail.com>
2016-01-14 10:36:40 +0100

    Odczytywanie parametrów klasyfikatorów cd.
    done

--- Blue-master/js/controllers/classifier-controller.js
+++ Blue-master/js/controllers/classifier-controller.js
@@ -4,20 +4,23 @@ angular.module("Blue").controller("ClassifierController",['$http', '$scope', fun
     algorithms : 
 	[
 		{
-			id : '1',
+			id : '0',
 			name: 'SVM',
+			algName: 'svm',
 			description: 'C-Support Vector Classification',
 			note: 'The implementation is based on libsvm. The fit time complexity is more than quadratic with the number of samples which makes it hard to scale to dataset with more than a couple of 10000 samples. The multiclass support is handled according to a one-vs-one scheme.',
 		},
 		{
-			id : '2',
+			id : '1',
 			name: 'NaiveBytes',
+			algName : 'naive',
 			description: '*description*',
 			note: '*note*',
 		},
 		{
-			id : '3',
+			id : '2',
 			name: 'DecisionTreeClassifier',
+			algName : 'dTree',
 			description: 'A decision tree classifier.',
 			note: 'Decision Trees (DTs) are a non-parametric supervised learning method used for classification and regression. The goal is to create a model that predicts the value of a target variable by learning simple decision rules inferred from the data features.',
 		},
@@ -26,33 +29,43 @@ angular.module("Blue").controller("ClassifierController",['$http', '$scope', fun
 	[
 	{
 			id : '0',
-			name: 'none'
+			name: 'none',
+			algName: 'none'
 
 		},
 		{
 			id : '1',
 			name: 'KFold',
+			algName: 'kFold',
 			description: 'K-Folds cross validation iterator.',
 			note: 'Provides train/test indices to split data in train test sets. Split dataset into k consecutive folds (without shuffling by default).\nEach fold is then used a validation set once while the k - 1 remaining fold form the training set.',
 		},
 		{
 			id : '2',
 			name: 'ShuffleSplit',
+			algName: 'shuffleSplit',
 			description: 'Random permutation cross-validation iterator.',
 			note: 'Yields indices to split data into training and test sets.\nContrary to other cross-validation strategies, random splits do not guarantee that all folds will be different, although this is still very likely for sizeable datasets.',
 		}
 	]
 	};
 
-	$scope.addNewClassifier = function(form)
+	$scope.addNewClassifier = function()
 	{
-		var name = $scope.classifierName;
-		var params = $scope.fields;
+		var params = $scope.fields[$scope.data.repeatSelect];
+		var lcparams = $scope.lcfields[$scope.data.repeatSelectLCurve];
+		var classifier_name = $scope.classifierName;
+		var classifier_type = $scope.data.algorithms[$scope.data.repeatSelect].algName;
+		var cross_validation_type = $scope.data.learningCurveAlgorithms[$scope.data.repeatSelectLCurve].algName;
 		$http({method: 'POST', 
 				url: '/dataprocessing/rest-api/', 	
 				data: {
-					name: $scope.classifierName,
-					params,
+					'classifier_name' : classifier_name,
+					'classifier_type' : classifier_type,
+					'classifier_params' : params,
+					'cross_validation_type' : cross_validation_type,
+					'cross_validation_params' : lcparams,
+					'collection_id' : 2,
 					},
 				headers: {               
 					'Content-Type': 'application/json'      
