d3312335149b0cda9137fadf64d0d95e2847c9fb
patryk1992 <patryk.bierla@gmail.com>
2016-01-14 13:36:10 +0100

    poprawki przycisku zaloguj i wyloguj

--- Blue-master/js/app.js
+++ Blue-master/js/app.js
@@ -1,1 +1,9 @@
-angular.module('Blue',['ngRoute','ngTable','ngCookies']);
+var myApp=angular.module('Blue',['ngRoute','ngTable','ngCookies']);
+
+myApp.constant('USER_ROLES', {
+  all: '*',
+  admin: 'ROLE_ADMIN',
+  expert: 'ROLE_EXPERT',
+  annotator: 'ROLE_ANNOTATOR',
+  banned: 'ROLE_BANNED'
+});
--- Blue-master/js/controllers/login-index-controller.js
+++ Blue-master/js/controllers/login-index-controller.js
@@ -1,11 +1,13 @@
-angular.module("Blue").controller("LoginIndexController", ['$scope', '$rootScope', '$location', 'AuthenticationService', '$http', 'Base64', '$window', "$cookieStore",
-    function($scope, $rootScope, $location, AuthenticationService, $http, Base64, $window, $cookieStore) {
+angular.module("Blue").controller("LoginIndexController", ['$scope', '$rootScope', '$location', 'AuthenticationService', '$http', 'Base64', '$window', '$cookieStore','USER_ROLES',
+    function($scope, $rootScope, $location, AuthenticationService, $http, Base64, $window, $cookieStore,USER_ROLES) {
         // reset login status
-        AuthenticationService.ClearCredentials();
+       // AuthenticationService.ClearCredentials();
         var controller = this;
         controller.login = login;
         controller.logout = logout;
         controller.isAuthorized = isAuthorized;
+        controller.checkRole = checkRole;
+
 
         function login() {
             AuthenticationService.ClearCredentials();
@@ -20,6 +22,7 @@ angular.module("Blue").controller("LoginIndexController", ['$scope', '$rootScope
                 }
             }).success(function(data) {
                 AuthenticationService.SetCredentials(controller.username, controller.password, data.roleName);
+                 $location.path('/classifiers');
             }).error(function(data) {
                 $window.alert(JSON.stringify(data));
             });
@@ -28,9 +31,19 @@ angular.module("Blue").controller("LoginIndexController", ['$scope', '$rootScope
 
         function logout() {
             AuthenticationService.ClearCredentials();
+             $location.path('/login');
         };
 
         function isAuthorized() {
+            var globals = $cookieStore.get('globals');
+            if (globals == undefined) {
+                return false;
+            } else {
+                return true;
+            }
+
+        };
+        function checkRole() {
             var globals = $cookieStore.get('globals');
             if (globals == undefined) {
                 return false;
